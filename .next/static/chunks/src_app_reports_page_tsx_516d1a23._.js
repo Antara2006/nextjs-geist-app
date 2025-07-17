776238805f0b30db468feb2c29cc756a1cb26d4e
Antara2006 <antarasantra2006@gmail.com>
2025-07-17 19:35:08 +0530

    Add .next/static/chunks/src_app_reports_page_tsx_516d1a23._.js 1752761109308

---
+++ .next/static/chunks/src_app_reports_page_tsx_516d1a23._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_15ce6fb9._.js",
+  "static/chunks/node_modules_tailwind-merge_dist_bundle-mjs_mjs_b854acb4._.js",
+  "static/chunks/node_modules_lodash_ff39e716._.js",
+  "static/chunks/node_modules_recharts_es6_ffed4bcc._.js",
+  "static/chunks/node_modules_9601e6e2._.js"
+],
+    source: "dynamic"
+});
